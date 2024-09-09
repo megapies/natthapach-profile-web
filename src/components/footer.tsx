bc936bd86cca0f1fd8fc241773711cfb99c2b58e
Natthapach <natthapach.an@chomchob.com>
2024-09-09 11:40:45 +0700

    src: decomponent footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+export function Footer() {
+  return (
+    <footer className="bg-[#2E2F3D] py-8">
+        <div className="container mx-auto px-4">
+          <div className="flex flex-wrap justify-between items-center">
+            <div className="w-full md:w-1/3 mb-4 md:mb-0">
+              <h3 className="text-lg font-semibold mb-2 text-[#9F8466]">Quick Links</h3>
+              <nav className="flex flex-wrap">
+                {['Home', 'About', 'Experience', 'Projects', 'Research', 'Skills', 'Education', 'Contact'].map((item) => (
+                  <Link key={item} href={`#${item.toLowerCase()}`} className="mr-4 hover:text-[#9F8466]">
+                    {item}
+                  </Link>
+                ))}
+              </nav>
+            </div>
+            <div className="w-full md:w-1/3 mb-4 md:mb-0 text-center">
+              <p>&copy; 2024 Natthapach Anuwattananon. All Rights Reserved.</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+  )
+}
--- src/components/improved-profile-website.tsx
+++ src/components/improved-profile-website.tsx
@@ -23,6 +23,7 @@ import { ReserchSection } from './section/research-section'
 import { SkillSection } from './section/skill-section'
 import { EducationSection } from './section/education-section'
 import { ContactSection } from './section/contact-section'
+import { Footer } from './footer'
 export function ImprovedProfileWebsite() {
   const [activeSection, setActiveSection] = useState('home')
   const [selectedProject, setSelectedProject] = useState<Project | null>(null)
@@ -77,25 +78,7 @@ export function ImprovedProfileWebsite() {
         <ContactSection fadeInUp={fadeInUp}/>
       </main>
 
-      <footer className="bg-[#2E2F3D] py-8">
-        <div className="container mx-auto px-4">
-          <div className="flex flex-wrap justify-between items-center">
-            <div className="w-full md:w-1/3 mb-4 md:mb-0">
-              <h3 className="text-lg font-semibold mb-2 text-[#9F8466]">Quick Links</h3>
-              <nav className="flex flex-wrap">
-                {['Home', 'About', 'Experience', 'Projects', 'Research', 'Skills', 'Education', 'Contact'].map((item) => (
-                  <Link key={item} href={`#${item.toLowerCase()}`} className="mr-4 hover:text-[#9F8466]">
-                    {item}
-                  </Link>
-                ))}
-              </nav>
-            </div>
-            <div className="w-full md:w-1/3 mb-4 md:mb-0 text-center">
-              <p>&copy; 2024 Natthapach Anuwattananon. All Rights Reserved.</p>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Footer/>
 
       <Dialog open={selectedProject !== null} onOpenChange={() => setSelectedProject(null)}>
         <DialogContent className="bg-[#2E2F3D] text-white border-[#9F8466]">
